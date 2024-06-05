05e81620ca68ce3d979809a0f98e4cd09105d9d1
Ghali Hassouni <ghalihassouni@gmail.com>
2024-06-05 11:28:44 +0200

    add username to user & remove wrong migration

--- db/migrate/20240604153822_remove_column_from_documents.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumnFromDocuments < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :documents, :image_url, :string
-  end
-end
--- db/migrate/20240604153844_remove_column_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumnFromUsers < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :users, :image_url, :string
-  end
-end
