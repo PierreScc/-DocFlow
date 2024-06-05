83e18f9272796f35bd80c6d3ef648f0c6a388030
thibault wanten <thibault.wanten@gmail.com>
2024-06-05 16:46:59 +0200

    Mercredi

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :groups, only: [:index, :show, :new, :update, :destroy, :create] do
+  resources :groups, only: [:index, :show, :new, :edit, :update, :destroy, :create] do
     resources :documents, only: [:show]
-    
-    resources :messages, only: [:create]
+    resources :messages, only: [:index, :create, :show]
     resources :user_groups, only: [:create]
   end
 
@@ -18,6 +14,13 @@ Rails.application.routes.draw do
     resources :comments, only: [:create]
     resources :assignements, only: [:new, :create]
   end
-  # Defines the root path route ("/")
-  # root "posts#index"
+
+  resources :users, only: [:show, :edit, :update]
+
+  resources :messages, only: [:index]
+
+  # Route pour le formulaire de soumission de groupe
+  get 'submit-group', to: 'groups#new', as: 'submit_group'
+  post 'submit-group', to: 'groups#create'
 end
+
