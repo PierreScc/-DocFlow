5b784028379052f93512a0c2c8e619efdd2c1a19
clement lin <clementlinn@outlook.fr>
2024-06-05 12:41:14 +0200

    cloudinary

--- app/models/document.rb
+++ app/models/document.rb
@@ -3,5 +3,5 @@ class Document < ApplicationRecord
   belongs_to :group
   has_many :comments, dependent: :destroy
   has_many :assignements, dependent: :destroy
-  has_many_attached :photos
+  has_one_attached :photo
 end
