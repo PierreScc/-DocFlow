203905b140063af1a6330a3ee1fee41250250971
clement lin <clementlinn@outlook.fr>
2024-06-06 11:20:34 +0200

    need pull

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,5 +1,7 @@
 import { Application } from "@hotwired/stimulus"
 import { faker } from '@faker-js/faker';
+import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap/dist/css/bootstrap'
 
 const application = Application.start()
 
@@ -8,4 +10,3 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
-
