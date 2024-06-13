e6b606df4bd2ca9205610491fe74cd21eb2e1421
clement lin <clementlinn@outlook.fr>
2024-06-13 18:19:37 +0200

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -37,15 +37,15 @@ file = URI.open("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjZep62a4
 user.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
 user.save!
 
-user = User.new(email: "rihanna@test.com", password: "123456", username: "Rihanna")
+rihanna = User.new(email: "rihanna@test.com", password: "123456", username: "Rihanna")
 file = URI.open("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrWn8rZENm5oOLxW46htiZAPxBjZ1KQdGIPhVjxPwFKKAKxz_TiF7uIV1D4Z5xZEj91DA&usqp=CAU")
-user.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
-user.save!
+rihanna.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+rihanna.save!
 
-user = User.new(email: "clement@test.com", password: "123456", username: "Clement")
+clement = User.new(email: "clement@test.com", password: "123456", username: "Clement")
 file = URI.open("https://ca.slack-edge.com/T02NE0241-U06TGAB5T7C-7927c68befc2-512")
-user.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
-user.save!
+clement.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+clement.save!
 
 user = User.new(email: "Ghali@test.com", password: "123456", username: "Ghali")
 file = URI.open("https://ca.slack-edge.com/T02NE0241-U06U343CF9U-e4f83f86e924-512")
@@ -68,27 +68,68 @@ puts "#{User.count} users created"
 
 # Create 5 documents
 puts "Creating groups..."
-companies = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQR1O2az2LIgoyvNvyLkGJHe14jr029tk0Mxg&s",
-             "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdlliBN0cVBM-ejlCnKuxh_rRNKvaSJDrfqw&s",
-             "https://previews.123rf.com/images/priyosu/priyosu1801/priyosu180100054/93457442-b%C3%A2timent-et-b%C3%A2timent-logo-design-vectoriel-logo-immobilier-logo-de-conception-pour-les.jpg",
-             "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZU-UTqsX64zzjCZENRUtNSZj9YfCfIoltCQ&s",
-             "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqjXck8Fj7LGW2IXrO-RHHybHFZvc-X9TGJw&s"]
+# companies = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQR1O2az2LIgoyvNvyLkGJHe14jr029tk0Mxg&s",
+#              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdlliBN0cVBM-ejlCnKuxh_rRNKvaSJDrfqw&s",
+#              "https://previews.123rf.com/images/priyosu/priyosu1801/priyosu180100054/93457442-b%C3%A2timent-et-b%C3%A2timent-logo-design-vectoriel-logo-immobilier-logo-de-conception-pour-les.jpg",
+#              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZU-UTqsX64zzjCZENRUtNSZj9YfCfIoltCQ&s",
+#              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqjXck8Fj7LGW2IXrO-RHHybHFZvc-X9TGJw&s"]
 
-companies.each do |company|
-  group = Group.new(
-    title: Faker::Company.department,
-    category: "Company"
-  )
-  file = URI.open(company)
-  group.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
-  group.save!
-
-  user_group = UserGroup.new(
-    user: User.all.sample,
-    group: group
-  )
-  user_group.save!
+group = Group.new(title: "F&F Riri", category: "Professional")
+group.save!
+User.all.sample(3).each do |user|
+  unless user == rihanna || user == clement
+    user_group = UserGroup.new(
+      user: user,
+      group: group
+    )
+    user_group.save!
+  end
 end
+UserGroup.create!(user: rihanna, group: group)
+UserGroup.create!(user: clement, group: group)
+
+group_1 = Group.new(title: "Sound Engineers", category: "Professional")
+group_1.save!
+User.all.sample(3).each do |user|
+  unless user == clement
+    user_group = UserGroup.new(
+      user: user,
+      group: group_1
+    )
+    user_group.save!
+  end
+end
+UserGroup.create!(user: clement, group: group_1)
+
+group_2 = Group.new(title: "Staff Prod", category: "Professional")
+group_2.save!
+User.all.sample(3).each do |user|
+  unless user == clement
+    user_group = UserGroup.new(
+      user: user,
+      group: group_2
+    )
+    user_group.save!
+  end
+end
+UserGroup.create!(user: clement, group: group_2)
+
+
+# companies.each do |company|
+#   group = Group.new(
+#     title: Faker::Company.department,
+#     category: "Company"
+#   )
+#   file = URI.open(company)
+#   group.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+#   group.save!
+
+#   user_group = UserGroup.new(
+#     user: User.all.sample,
+#     group: group
+#   )
+#   user_group.save!
+# end
 
 puts "#{Group.count} groups created"
 
