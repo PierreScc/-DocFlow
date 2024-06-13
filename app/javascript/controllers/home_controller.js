b5dcc4ce0f80632144dc1b563fa1037c66478a5b
Ghali Hassouni <ghalihassouni@gmail.com>
2024-06-13 15:40:06 +0200

    modifdiane

--- app/javascript/controllers/home_controller.js
+++ app/javascript/controllers/home_controller.js
@@ -2,6 +2,11 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="home"
 export default class extends Controller {
-  static targets = [""]
+  static targets = ["chat"]
 
+  scrollDown() {
+    console.log(this.chatTarget.scrollTop);
+    this.chatTarget.scrollTo(0, this.chatTarget.scrollHeight);
+    console.log(this.chatTarget.scrollTop);
+  }
 }
